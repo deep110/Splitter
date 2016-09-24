13e18848d93d94aa0f741bba3136d7e68fb09ecf
deep-154 <deepankar11093@gmail.com>
2016-09-24 20:18:41 +0530

    standard assets imported and background made

---
+++ Assets/Scripts/InputMapper.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputMapper : MonoBehaviour {
+
+	
+	void Update () {
+		
+		for(int i=0;i< Input.touchCount && i<2;i++){
+			Debug.Log(""+Input.GetTouch(i).position);
+		}
+	}
+}
