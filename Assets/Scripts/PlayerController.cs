7e6186dd36eb18bf8e68ea5fc1c0753204ef4357
deep-154 <deepankar11093@gmail.com>
2016-09-26 19:03:47 +0530

    basic movement complete

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,43 +8,75 @@ namespace Splitter.Player{
 
 	public class PlayerController : MonoBehaviour{
 
-		public readonly float SPEED = 8;
+		public readonly float SPEED = 20;
 		private Pair<Transform> player;
-		private Pair<Rigidbody2D> rigidbody;
-		private Pair<Vector2> velocity;
+		private Pair<Rigidbody2D> rigidBody;
+		private Pair<Vector3> initialPos;
+
+		private Pair<float> velocity;
+		
 
 		void Start(){
-			//Debug.Log(transform.FindChild("Left"));
 			player = new Pair<Transform>(transform.Find("Left"), transform.Find("Right"));
-			rigidbody = new Pair<Rigidbody2D>(player.Left.GetComponent<Rigidbody2D>(), player.Right.GetComponent<Rigidbody2D>());
-			velocity = new Pair<Vector2>(new Vector2(0,0), new Vector2(0,0));
+			initialPos = new Pair<Vector3>(player.Left.position, player.Right.position);
+			rigidBody = new Pair<Rigidbody2D>(player.Left.GetComponent<Rigidbody2D>(), player.Right.GetComponent<Rigidbody2D>());
+			velocity = new Pair<float>(0,0);
 		}
 
 		public void Move(InputManager.InputType input){
-			
+			//Debug.Log(input);
+
 			switch(input){
 				case InputManager.InputType.LEFT:
-					velocity.Left = new Vector2(-1, 0);
-					velocity.Right = new Vector2(-1, 0);
+					velocity.Left = -1;
+					velocity.Right = -1;
 					break;
 
 				case InputManager.InputType.RIGHT:
-					velocity.Left = new Vector2(1, 0);
-					velocity.Right = new Vector2(1, 0);
+					velocity.Left = 1;
+					velocity.Right = 1;
 					break;
 
 				case InputManager.InputType.BOTH:
-					velocity.Left = new Vector2(-1, 0);
-					velocity.Right = new Vector2(1, 0);
+					velocity.Left = -1;
+					velocity.Right = 1;
 					break;
 
 				case InputManager.InputType.NONE:
+					velocity.Left = getReturnVelocity(initialPos.Left.x, player.Left.position.x);
+					velocity.Right = getReturnVelocity(initialPos.Right.x, player.Right.position.x);
 
+					if((int)velocity.Left == 0){
+						player.Left.position = initialPos.Left;
+					}
+
+					if((int)velocity.Right == 0){
+						player.Right.position = initialPos.Right;
+					}
 					break;
 			}
 			
-			rigidbody.Left.velocity = velocity.Left * SPEED;
-			rigidbody.Right.velocity = velocity.Right * SPEED;
+			rigidBody.Left.velocity = new Vector2(velocity.Left, 0) * SPEED;
+			rigidBody.Right.velocity = new Vector2(velocity.Right, 0) * SPEED;
+		}
+
+		/**
+		* x1 = initial Position
+		* x2 = current Position
+		**/
+		private float getReturnVelocity(float x1, float x2){
+			float v;
+			if(x2-x1 <0){
+				v = 1;
+			}else if(x2 - x1 > 0){
+				v = -1;
+			}else v = 0;
+
+			if(Mathf.Abs(x2-x1) <= (SPEED/50)){
+				v = 0;
+			}
+
+			return v;
 		}
 
 	}
