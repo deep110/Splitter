58ae58663d12c503eabf222f9ad2b91421020f14
deep-154 <deepankar11093@gmail.com>
2016-10-11 13:18:26 +0530

    side wall sparks created

--- Assets/Scripts/Player/PlayerScript.cs
+++ Assets/Scripts/Player/PlayerScript.cs
@@ -2,8 +2,25 @@
 
 public class PlayerScript : MonoBehaviour {
 
+	public ParticleSystem sparksTop;
+	public ParticleSystem sparksBottom;
+
+	private bool isPlaying = false;
+
 	void OnCollisionEnter2D(Collision2D other) {
-       // Debug.Log("player collider");
+        if(other.transform.tag.Equals("Wall")){
+      		sparksTop.Play();
+      		sparksBottom.Play();
+      		isPlaying = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D other){
+    	if(isPlaying && other.transform.tag.Equals("Wall")){
+    		sparksTop.Stop();
+    		sparksBottom.Stop();
+    		isPlaying = false;
+    	}
     }
 
 }
