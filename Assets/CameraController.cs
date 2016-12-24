0747adf3e12765ee9a1419295fe342f630c1f208
harshit211997 <harshit211997@gmail.com>
2016-12-24 18:42:20 +0530

    rfac: Makes the spark go off on game over

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -15,6 +15,7 @@ public class CameraController : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
 		//the player position is the mean of positions of left and right childs
+
 		playerPosition = new Vector3((player.transform.GetChild (0).position.x + player.transform.GetChild (1).position.x) / 2,
 			0,
 			0);
