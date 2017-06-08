18aed13c4d6935d4873114df5053305e38961c31
Deepankar Agrawal <deepankar11093@gmail.com>
2017-06-08 21:17:29 +0530

    initial score problem removed

--- Assets/Scripts/GamePlay/GamePlayManager.cs
+++ Assets/Scripts/GamePlay/GamePlayManager.cs
@@ -25,7 +25,7 @@ public class GamePlayManager : MonoBehaviour {
 	void OnTriggerExit2D(Collider2D other){
 		if(other.tag.Equals("SpikeBoth")) {
 			score += 0.5f;
-		} else {
+		} else if(other.tag.Equals("SpikeOther")){
 			score++;
 		}
 		if (score > PlayerPrefs.GetInt ("highscore")) {
