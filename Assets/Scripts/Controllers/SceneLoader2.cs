e92cc005e0b582e39d388eb9cf2fc3b9dab6ded1
Deepankar Agrawal <deepankar11093@gmail.com>
2017-06-08 20:56:47 +0530

    fade error resolved at start

--- Assets/Scripts/Controllers/SceneLoader2.cs
+++ Assets/Scripts/Controllers/SceneLoader2.cs
@@ -6,7 +6,7 @@ public class LoadScene : StateMachineBehaviour {
 
 	 // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
 	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-		SceneManager.LoadScene (1); 
+		//SceneManager.LoadScene (1); 
 	}
 
 	// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
---
+++ Assets/Scripts/Utils/SceneLoader.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+	public void LoadScene (int level){
+		SceneManager.LoadScene(level);
+	}
+}
